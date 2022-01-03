3ab6ffa5db5c6ca1fba03704f2c0d6dfde176f8d
ImGMin <76603110+ImGMin@users.noreply.github.com>
2022-01-03 22:06:18 +0900

    2022.01.03

--- Assets/2.Scripts/O_become.cs
+++ Assets/2.Scripts/O_become.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class O_become : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject prefab;
-    public Transform parent;
+    public GameObject animal_1;
+    public GameObject animal_2;
+    public GameObject animal_3;
     void Start()
     {
         
@@ -17,7 +18,21 @@ public class O_become : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            GameObject inst = Instantiate(prefab, new Vector2(0,0), Quaternion.identity);
+            GameObject Instant =  Instantiate(animal_1, new Vector2(-5,0), Quaternion.identity);
+            Instant.name = "animal_1";
+            Instant.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GameObject Instant = Instantiate(animal_2, new Vector2(-5, 0), Quaternion.identity);
+            Instant.name = "animal_2";
+            Instant.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GameObject Instant = Instantiate(animal_3, new Vector2(-5, 0), Quaternion.identity);
+            Instant.name = "animal_3";
+            Instant.SetActive(true);
         }
     }
 }
--- Assets/2.Scripts/O_move.cs
+++ Assets/2.Scripts/O_move.cs
@@ -7,6 +7,11 @@ public class O_move : MonoBehaviour
     public float speed;
     float speed_init;
     bool area; //영역안에 상대가 있는지
+    public Animator animator;
+    public Transform pos;
+    public Vector2 boxsize;
+    private float curtime;
+    public float cooltime = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,11 +27,29 @@ public class O_move : MonoBehaviour
     {
         if (area == true)
         {
-            speed = 0F;
+            speed = 0F; 
+                // 공격 or  적 체력 -= 데미지                 
+            if (curtime <= 0)                
+            {                    
+                Collider2D[] collider2D = Physics2D.OverlapBoxAll(pos.position, boxsize, 0);                
+                foreach (Collider2D collider in collider2D)                
+                {                
+                    Debug.LogError(collider.tag);                    
+                }                
+                curtime = cooltime;                
+                animator.SetTrigger("attack");
+                animator.SetBool("run", false);
+            }            
+            else
+            {                   
+                curtime -= Time.deltaTime;
+                animator.ResetTrigger("attack");
+            }
         }
         if (area == false)
         {
             speed = speed_init;
+            animator.SetBool("run", true);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
