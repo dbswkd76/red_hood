0c642ff055639c57154a57ce29bd5f12f4df9d40
dbswkd76 <dbswkd76@naver.com>
2022-01-03 22:15:46 +0900

    01.03

---
+++ Assets/2.Scripts/Panel_GameOver.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Panel_GameOver : MonoBehaviour
+{
+    
+    private void Awake()
+    {
+        transform.gameObject.SetActive(false); 
+    }
+
+    public void Show()
+    {
+        
+        transform.gameObject.SetActive(true);
+    }
+
+    public void OnClick_Retry() // 재도전 함수
+    {
+        SceneManager.LoadScene("yunjang"); 
+    }
+   
+}
--- Assets/2.Scripts/Player.cs
+++ Assets/2.Scripts/Player.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class Player : Character
 {
   
+    
     [SerializeField]
-    private State health;
-
+    public State mana;
     [SerializeField]
-    private State mana;
+    public State health;
+    [SerializeField]
+    protected float initHealth;
+    public Panel_GameOver panel_GameOver; // 게임오버 판넬
+    public bool die;
 
     Animator animator;
-    private float initHealth = 100;
+    
     private float initMana = 50;
 
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
+        die = false;
         direction = Vector2.zero;
         health.Intialize(initHealth, initHealth);
         mana.Intialize(initMana, initMana);
@@ -33,90 +38,115 @@ public class Player : Character
         GetInput();
     
         base.Update();
-
-        float horizontal  = Input.GetAxis("Horizontal")  ; //좌우 방향키 입력
-        float offset = 0.5f + Input.GetAxis("Sprint")  ;
-        float moveParameter = Mathf.Abs(horizontal * offset ); //파라미터 절대값 적용
-        animator.SetFloat("RunState", moveParameter);
-
-
-        if(Input.GetAxisRaw("Horizontal") < 0) //플레이어 이미지 좌우반전
+        if (die == false)
         {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
-        else if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        }
-        
-       // if(Input.ke)
+            float horizontal = Input.GetAxis("Horizontal"); //좌우 방향키 입력
+            float offset = 0.5f + Input.GetAxis("Sprint");
+            float moveParameter = Mathf.Abs(horizontal * offset); //파라미터 절대값 적용
+            animator.SetFloat("RunState", moveParameter);
 
-    }
-   
+
+            if (Input.GetAxisRaw("Horizontal") < 0) //플레이어 이미지 좌우반전
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+        if (health.MyCurrentValue <= 0)
+        {
+            StartCoroutine(Die());
+
+        }
+
+        }
+
     private void GetInput()
     {
-        Vector2 moveVector; //키보드 입력
+        if (die == false)
+        {
+            Vector2 moveVector; //키보드 입력
 
-        moveVector.x = Input.GetAxisRaw("Horizontal");
-        moveVector.y = Input.GetAxisRaw("Vertical");
+            moveVector.x = Input.GetAxisRaw("Horizontal");
+            moveVector.y = Input.GetAxisRaw("Vertical");
 
-        direction = moveVector;
+            direction = moveVector;
 
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            health.MyCurrentValue -= 10;
-            mana.MyCurrentValue -= 10;
-        }
-        if (Input.GetKeyDown(KeyCode.O))
-        {
-            health.MyCurrentValue += 10;
-            mana.MyCurrentValue += 10;
-        }
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                health.MyCurrentValue -= 10;
+                mana.MyCurrentValue -= 10;
+            }
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                health.MyCurrentValue += 10;
+                mana.MyCurrentValue += 10;
+            }
 
-        if (Input.GetKeyDown(KeyCode.Z))  //기본공격
-        {
-            animator.SetTrigger("Attack");
-            animator.SetFloat("AttackState", 0);
-            animator.SetFloat("NormalState", 0);
-            animator.SetFloat("SkillState", 0);
+            if (Input.GetKeyDown(KeyCode.Z))  //기본공격
+            {
+                animator.SetTrigger("Attack");
+                animator.SetFloat("AttackState", 0);
+                animator.SetFloat("NormalState", 0);
+                animator.SetFloat("SkillState", 0);
+            }
+            if (Input.GetKeyDown(KeyCode.X))  //활공격
+            {
+                animator.SetTrigger("Attack");
+                animator.SetFloat("AttackState", 0);
+                animator.SetFloat("NormalState", 0.5f);
+                animator.SetFloat("SkillState", 0);
+            }
+            if (Input.GetKeyDown(KeyCode.C))  //마법공격
+            {
+                animator.SetTrigger("Attack");
+                animator.SetFloat("AttackState", 0);
+                animator.SetFloat("NormalState", 1);
+                animator.SetFloat("SkillState", 0);
+            }
+            if (Input.GetKeyDown(KeyCode.Q))  //기본스킬
+            {
+                animator.SetTrigger("Attack");
+                animator.SetFloat("AttackState", 1);
+                animator.SetFloat("NormalState", 0);
+                animator.SetFloat("SkillState", 0);
+            }
+            if (Input.GetKeyDown(KeyCode.W))  //활스킬
+            {
+                animator.SetTrigger("Attack");
+                animator.SetFloat("AttackState", 1);
+                animator.SetFloat("NormalState", 0);
+                animator.SetFloat("SkillState", 0.5f);
+            }
+            if (Input.GetKeyDown(KeyCode.E))  //마법스킬
+            {
+                animator.SetTrigger("Attack");
+                animator.SetFloat("AttackState", 1);
+                animator.SetFloat("NormalState", 0);
+                animator.SetFloat("SkillState", 1);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.X))  //활공격
-        {
-            animator.SetTrigger("Attack");
-            animator.SetFloat("AttackState", 0);
-            animator.SetFloat("NormalState", 0.5f);
-            animator.SetFloat("SkillState", 0);
+            
         }
-        if (Input.GetKeyDown(KeyCode.C))  //마법공격
-        {
-            animator.SetTrigger("Attack");
-            animator.SetFloat("AttackState", 0);
-            animator.SetFloat("NormalState", 1);
-            animator.SetFloat("SkillState", 0);
-        }
-        if (Input.GetKeyDown(KeyCode.Q))  //기본스킬
-        {
-            animator.SetTrigger("Attack");
-            animator.SetFloat("AttackState", 1);
-            animator.SetFloat("NormalState", 0);
-            animator.SetFloat("SkillState", 0);
-        }
-        if (Input.GetKeyDown(KeyCode.W))  //활스킬
-        {
-            animator.SetTrigger("Attack");
-            animator.SetFloat("AttackState", 1);
-            animator.SetFloat("NormalState", 0);
-            animator.SetFloat("SkillState", 0.5f);
-        }
-        if (Input.GetKeyDown(KeyCode.E))  //마법스킬
-        {
-            animator.SetTrigger("Attack");
-            animator.SetFloat("AttackState", 1);
-            animator.SetFloat("NormalState", 0);
-            animator.SetFloat("SkillState", 1);
-        }
+    IEnumerator Die()
+    {
+        die = true;
+        animator.SetTrigger("Die");
+        yield return new WaitForSeconds(2f);
+        GameOver();
     }
+    
+
+
+    public void GameOver()
+    {
+        panel_GameOver.Show();
+    }
+}
+    
     
     // Update is called once per frame
     
-}
+
