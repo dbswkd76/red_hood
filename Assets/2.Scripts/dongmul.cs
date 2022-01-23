798851142ff2b8c96b53cafa128633493c915dbc
meoheee <dany1124@hanmail.net>
2022-01-24 03:02:19 +0900

    dongmulcoding

---
+++ Assets/2.Scripts/dongmul.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dongmul : MonoBehaviour
+{
+    struct dongmul_clear {
+        public bool bear = false;
+        public bool deer = false;
+        public bool eagle = false;
+        public bool fox = false;
+        public bool meerkat = false;
+        public bool panther = false;
+        public bool raven = false;
+        public bool snake = false;
+        public bool wolf = false;
+    };
+    struct bear {
+        bool deck = false;
+    };
+
+    struct deer {
+        bool deck = false;
+    };
+
+    struct eagle{
+        bool deck = false;
+    };
+
+    struct fox{
+        bool deck = false;
+    };
+
+    struct meerkat{
+        bool deck = false;
+    };
+
+    struct panther{
+        bool deck = false;
+    };
+
+    struct raven{
+        bool deck = false;
+    };
+
+    struct snake{
+        bool deck = false;
+    };
+
+    struct wolf{
+        bool deck = false;
+    };
+
+}
