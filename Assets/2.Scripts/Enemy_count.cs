19222e25386056dd7ffaab14a8c13ed8eb35bdf7
junssg <80884697+junssg@users.noreply.github.com>
2022-02-12 22:05:46 +0900

    wolf count

--- Assets/2.Scripts/Enemy_count.cs
+++ Assets/2.Scripts/Enemy_count.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy_count : MonoBehaviour
 {
     public static int died_wolf;
     [SerializeField] int wolves;
+    [SerializeField] Text left_wolf;
     [SerializeField] GameObject Clearpanel;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,12 @@ public class Enemy_count : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int a = wolves - died_wolf;
+        left_wolf.text ="남은 늑대 : "+ a.ToString()+"마리";
         if (died_wolf == wolves)
+        {
             Clearpanel.SetActive(true);
+            left_wolf.gameObject.SetActive(false);
+        }
     }
 }
