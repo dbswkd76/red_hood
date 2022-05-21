b57517d724d31f8e5cc087756636cc4ffe533ca1
dbswkd76 <dbswkd76@naver.com>
2022-05-21 17:02:35 +0900

    05.21.02

--- Assets/2.Scripts/Attack.cs
+++ Assets/2.Scripts/Attack.cs
@@ -11,7 +11,7 @@ public class Attack : MonoBehaviour
     public Transform pos;
     public Vector2 boxSize;
     public Enemy enemy;
-
+    [SerializeField] GameObject melee;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,17 +27,21 @@ public class Attack : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.Z))
             {
+                melee.SetActive(true);
+                Invoke("meleeoff", 0.5f);
                 Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(pos.position, boxSize, 0);
                 foreach (Collider2D collider in collider2Ds)
                 {
                     Debug.Log(collider.tag);
-                   // enemy.EnemyDamaged(10);
+                   
+                        
                 }
                 animator.SetTrigger("Attack");
                 animator.SetFloat("AttackState", 0);
                 animator.SetFloat("NormalState", 0);
                 animator.SetFloat("SkillState", 0);
                 curTime = coolTime;
+
             }
         }
         else
@@ -45,6 +49,10 @@ public class Attack : MonoBehaviour
             curTime -= Time.deltaTime;
         }
        
+    }
+    void meleeoff()
+    {
+        melee.SetActive(false);
     }
      void OnDrawGizoms()
     {
